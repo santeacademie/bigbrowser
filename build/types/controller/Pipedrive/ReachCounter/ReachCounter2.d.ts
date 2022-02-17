362c7a207370f12ece92936893a243d6d74a046e
JRK <jessym@santeacademie.com>
2022-02-17 22:46:14 +0100

    build 100%

--- build/types/controller/Pipedrive/ReachCounter/ReachCounter2.d.ts
+++
@@ -1,6 +0,0 @@
-import TamperController from '../../TamperController';
-import TamperRequest from '../../../core/router/TamperRequest';
-declare class ReachCounter2 extends TamperController {
-    run: (request: TamperRequest) => void;
-}
-export default ReachCounter2;
