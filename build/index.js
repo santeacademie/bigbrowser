ad361a8a2b73170601ef60929ea4aed0fd324699
tagadabot <tagadabot@santeacademie.com>
2022-05-18 17:16:57 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         @santeacademie/bigbrowser
 // @namespace    http://tampermonkey.net/
-// @version      3.7.18
+// @version      3.7.19
 // @description  Big browser is not watching you
 // @source       https://github.com/santeacademie/bigbrowser
 // @author       JRK (https://github.com/jr-k)
