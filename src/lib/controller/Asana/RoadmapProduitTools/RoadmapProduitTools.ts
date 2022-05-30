11426aa4be66fc73728e6e8934a739db73a348cf
JRK <jessym@santeacademie.com>
2022-05-30 17:38:46 +0200

    asana files

---
+++ src/lib/controller/Asana/RoadmapProduitTools/RoadmapProduitTools.ts
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+import TamperController from '../../../core/controller/TamperController';
+import TamperRequest from '../../../core/router/TamperRequest';
+import {ElementMarkerCallback} from '../../../core/presence/PresenceState';
+
+class RoadmapProduitTools extends TamperController {
+	run = (request: TamperRequest): void => {
+
+	};
+
+
+}
+
+export default RoadmapProduitTools;
