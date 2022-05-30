a4055ea1c59840b7db1126664b2ff89f5575d597
tagadabot <tech@santeacademie.com>
2022-05-30 15:41:23 +0000

    Build update

--- src/lib/controller/Asana/RoadmapProduitTools/RoadmapProduitTools.ts
+++ src/lib/controller/Asana/RoadmapProduitTools/RoadmapProduitTools.ts
@@ -5,11 +5,7 @@ import TamperRequest from '../../../core/router/TamperRequest';
 import {ElementMarkerCallback} from '../../../core/presence/PresenceState';
 
 class RoadmapProduitTools extends TamperController {
-	run = (request: TamperRequest): void => {
-
-	};
-
-
+	run = (request: TamperRequest): void => {};
 }
 
 export default RoadmapProduitTools;
