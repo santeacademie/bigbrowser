9b171393472c49c480d933d02d544a306a7eaafa
JRK <jessym@santeacademie.com>
2022-06-08 00:15:35 +0200

    code style

--- src/lib/controller/Pipedrive/ContentStudioTools/ContentStudioTools.ts
+++ src/lib/controller/Pipedrive/ContentStudioTools/ContentStudioTools.ts
@@ -1,8 +1,8 @@
 
 import TamperController from 'core/controller/TamperController';
 import TamperRequest from 'core/router/TamperRequest';
-import WssqlBridge from "../../../core/wssql/WssqlBridge";
-import {WssqlRemoteDatabase} from "../../../core/wssql/WssqlRemoteDatabase";
+import WssqlBridge from 'core/wssql/WssqlBridge';
+import {WssqlRemoteDatabase} from 'core/wssql/WssqlRemoteDatabase';
 
 class ContentStudioTools extends TamperController {
 	run = (request: TamperRequest): void => {
